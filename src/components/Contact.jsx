2ff11e6b04cb7e15f04f16a677cf4869455c882e
AMIREDDYRAKEY <rakeyr213@gmail.com>
2025-11-09 02:25:54 +0530

    Contact from changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,20 +15,27 @@ const Contact = () => {
 
       {/* Contact Info */}
       <div className="mt-10 bg-[#1a1a1a] p-8 rounded-2xl border border-[#2a2a2a] shadow-lg flex flex-col gap-6">
+        {/* Email */}
         <div className="flex items-center gap-3">
           <FaEnvelope className="text-[#ff014f] text-xl" />
           <p className="text-gray-300">rakeyr213@gmail.com</p>
         </div>
+
+        {/* Phone */}
         <div className="flex items-center gap-3">
           <FaPhoneAlt className="text-[#ff014f] text-xl" />
           <p className="text-gray-300">+91 6305541105</p>
         </div>
+
+        {/* Location */}
         <div className="flex items-center gap-3">
           <FaMapMarkerAlt className="text-[#ff014f] text-xl" />
-          <p className="text-gray-300">Andhra Pradesh, India,Kadapa(Dist),Duvur(M),ChinnaSinganapalli</p>
+          <p className="text-gray-300">
+            Andhra Pradesh, India, Kadapa(Dist), Duvur(M), ChinnaSinganapalli
+          </p>
         </div>
 
-         
+        
       </div>
     </div>
   );
